7df455e883ac5c184d68999022d4e947ad6c78bf
rwothoromo elijah <rwothoromo elijah>
2017-04-18 16:52:32 +0300

    Car class oop

---
+++ car_class_lab/car.py
@@ -0,0 +1,41 @@
+class Car(object):
+    def __init__(self,type=None,model='GM',name='General'):
+        self.type = type
+        self._model = model
+        self._name  = name
+        self._num_of_doors=4
+        self._num_of_wheels=4
+        self.speed = 0
+
+    @property
+    def name(self):
+        return self._name
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def num_of_doors(self):
+        if self.type=='Porshe' or self.type == 'Koenigsegg':
+            return 2
+        else:
+            return 4
+    @property
+    def num_of_wheels(self):
+        if self._name=='trailer':
+            return 8
+        else:
+            return 4 
+    def is_saloon(self):
+        if self._name !='trailer':
+            return 'saloon'
+        else:
+            return 'trailer'
+    def drive(self,speed):
+        if self.name=='trailer':
+            self.speed=77
+        else:
+            self.speed=1000
+        return self
+
+Mark as read (esc)
