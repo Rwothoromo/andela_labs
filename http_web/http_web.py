7f38933202feb72772b8ff29545e302fab32790c
rwothoromo elijah <rwothoromo elijah>
2017-04-19 15:43:09 +0300

    HTTP WEB API solution

---
+++ http_web/http_web.py
@@ -0,0 +1,24 @@
+import requests
+print("Capture User Login Data and Give an output")
+sitename = input("Enter username (Facebook): ")
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if sitename.lower() == 'facebook':
+    url = "https://www.facebook.com/login"
+
+page_get = requests.get(url, auth=(username, password))
+page_post = requests.post(url, {"username": username, "password": password})
+# print(page_get.text)#, page_post.text)
+
+results_dict = {}
+results_dict['Content_type'] = page_get.headers['content-type']
+# results_dict['Content'] = page_get.text
+results_dict['Encoding'] = page_get.encoding
+# results_dict['Json'] = page_post.json()
+results_dict['Status'] = page_post.status_code
+
+print("\nConnection details:\n\n")
+
+for item in results_dict:
+    print(item, " : ", results_dict[item])
