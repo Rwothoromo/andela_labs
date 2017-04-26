87edb5de17c7db51e27de0781b1a95fce1e0c631
rwothoromo elijah <rwothoromo elijah>
2017-04-26 11:48:31 +0300

    Removed commented out code

--- http_web/http_web.py
+++ http_web/http_web.py
@@ -7,8 +7,6 @@ sitename = input(r"Enter site (e.g. GitHub): ")
 if sitename.lower() == 'github':
     url = "https://api.github.com"
 
-# elif sitename.lower() == 'twitter':
-#     url = "https://twitter.com/login"
 else:
     print("Not supported")
     exit()
@@ -19,38 +17,6 @@ password = input(r"Enter password: ")
 log_url = url+'/users/'+username+'?'
 profile = requests.get(log_url, auth=(username, password))
 
-# print("\nName: ",profile.json()['name'], "\nBio: ", profile.json()['bio'])
 results_dict = profile.json()
 for key in results_dict:
     print(key, " : ", results_dict[key])
-
-
-# page_get = requests.get(url, auth=(username, password))
-# page_post = requests.post(url, {"username": username, "password": password})
-# print(page_get.text)#, page_post.text)
-
-# status = page_post.status_code
-# logged_in = "https://api.github.com"
-# profile = requests.get(logged_in, auth=(username, password))
-# print(profile.json())
-
-# if status != 200:
-#     print("Check username and password!")
-# else:
-#     print(username, page_post.status_code, " logged in successfully")
-
-
-
-
-# results_dict = {}
-# results_dict['Content_type'] = page_get.headers['content-type']
-# results_dict['Status'] = page_post.status_code
-# results_dict['Encoding'] = page_get.encoding
-# results_dict['Content'] = page_get.text
-# results_dict['Content'] = page_post.json()
-# results_dict['Json'] = page_post.json()
-# print(page_post.json())
-# print("\nConnection details:\n\n")
-
-# for key, value in results_dict:
-#     print(key, " : ", value)
